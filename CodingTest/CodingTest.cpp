cf18eb0850e3c61f581e92823b7131255d6fb03b
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-27 01:25:08 +0900

    endl -> '\n'으로 변경해서 성공 (상세이유 코드에 기재) (해결)

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -6,38 +6,35 @@ using namespace std;
 
 int main()
 {
-
 	//값 초기화
 	int iCount(0);
-	int iNumber(0);
+	int iPosX(0);
+	int iPosY(0);
 
-	//이중 vector문으로 초기화 
-	vector<vector<int>> vPos;
+	//vector <pair>로 정렬
+	vector<pair<int, int>> vPos;
 	cin >> iCount;
-
-	//사이즈 초기화
-	vPos.resize(iCount);
 
 	//값 대입
 	for (int i = 0; i < iCount; ++i)
 	{
-		for(int k = 0 ; k < 2 ; ++k)
-		{ 
-			cin >> iNumber;
-			vPos.at(i).push_back(iNumber);
-		}
+		cin >> iPosX >> iPosY;
+		vPos.push_back({ iPosX  , iPosY });
 	}
 
-	//람다식을 활용해서 더 빠르게 시도
+	//정렬 알고리즘
 	sort(vPos.begin(), vPos.end());
 
 	//값 출력
-
 	for (int i = 0; i < vPos.size(); ++i)
 	{
-		cout << vPos.at(i).at(0) << " " << vPos.at(i).at(1) << endl;
+		cout << vPos.at(i).first << " " << vPos.at(i).second << '\n';
 	}
-	
+
+	/*
+	'\n'을 사용하여 개행을 처리합니다.
+		'\n'은 endl보다 빠릅니다. endl은 개행 문자 출력과 함께 출력 버퍼를 강제로 flush하므로 불필요한 작업이 추가될 수 있습니다.
+	*/
 
 	return 0;
 }
