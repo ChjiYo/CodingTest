d316ddee02cada0203565d79363bfebb077658d1
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-28 18:38:59 +0900

    1193번 분수찾기 (성공)

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,40 +1,45 @@
 using namespace std;
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 int main()
 {
-	//값 초기화
-	int iCount(0);
-	int iPosX(0);
-	int iPosY(0);
+    
+    int iCount(0);
+    int iAdd(0);
+    int iIndex(0);
 
-	//vector <pair>로 정렬
-	vector<pair<int, int>> vPos;
-	cin >> iCount;
+    cin >> iCount;
 
-	//값 대입
-	for (int i = 0; i < iCount; ++i)
-	{
-		cin >> iPosX >> iPosY;
-		vPos.push_back({ iPosX  , iPosY } );
-	}
+    for (int i = 1; i <= iCount; ++i)
+    {
+        iAdd += i;
+        if (iAdd >= iCount)
+        {
+            iIndex = i;
+            break;
+        }
 
-	//정렬 알고리즘
-	sort(vPos.begin(), vPos.end());
+    }
 
-	//값 출력 
-	for (int i = 0; i < vPos.size(); ++i)
-	{
-		cout << vPos.at(i).first << " " << vPos.at(i).second << '\n';
-	}
+    int iFirst(0);
+    int iSecond(0);
 
-	/*
-	'\n'을 사용하여 개행을 처리합니다.
-		'\n'은 endl보다 빠릅니다. endl은 개행 문자 출력과 함께 출력 버퍼를 강제로 flush하므로 불필요한 작업이 추가될 수 있습니다.
-	*/
+    int iPrev = ((iIndex - 1) * (2 + (iIndex - 2))) * 0.5f;
 
-	return 0;
+    int iValue = iCount - iPrev - 1;
+    if (iIndex % 2 == 0)
+    {
+        iSecond = iIndex - iValue;
+        iFirst = 1 + iValue;
+    }
+    else
+    {
+        iFirst = iIndex - iValue;
+        iSecond = 1 + iValue;
+    }
+
+    cout << iFirst << "/" << iSecond;
+
+    return 0;
 }
