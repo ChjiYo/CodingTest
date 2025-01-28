bd6e3b4b34dc3d18e276ab208ac0e71f2169b6bf
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-28 22:54:54 +0900

    9012번 괄호 vector안에 string 값을 넣고 하나씩 erase로 삭제하면서 YES,NO값으로 반환

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,55 +1,42 @@
 using namespace std;
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main()
 {
-    //값 초기화
-    int iCount(0);
-    int iAdd(0);
-    int iIndex(0);
+	//값 초기화
+	int iCount(0);
+	string strPair;
+	vector<string> vecPair;
 
-    cin >> iCount;
-
-    //iAdd에 값을 더해서 first / second 중에 높은 숫자를 찾음
-    for (int i = 1; i <= iCount; ++i)
-    {
-        iAdd += i;
-        if (iAdd >= iCount)
-        {
-            iIndex = i;
-            break;
-        }
-
-    }
-
-    //값 초기화
-    int iFirst(0);
-    int iSecond(0);
-
-    //이전 최대값의 최대 n번째 값을 찾아서 뺌
-    int iPrev = ((iIndex - 1) * (2 + (iIndex - 2))) * 0.5f;
-
-    //iPrev에서 최대값을 뺴고 최소값을 더해서 값을 구함
-    //iIndex가 짝수일 때와 홀수일때 최소,최댓값이 다름
-    int iValue = iCount - iPrev - 1;
-    if (iIndex % 2 == 0)
-    {
-        iSecond = iIndex - iValue;
-        iFirst = 1 + iValue;
-    }
-    else
-    {
-        iFirst = iIndex - iValue;
-        iSecond = 1 + iValue;
-    }
-
-    //값 출력
-    cout << iFirst << "/" << iSecond;
+	cin >> iCount;
 
 
+	//vector에 값 삽입
+	for (int i = 0; i < iCount; ++i)
+	{
+		cin >> strPair;
+		vecPair.push_back(strPair);
+	}
 
 
+	//vector.at(i)안에 "()" 문자열이 있으면 그 문자를 삭제하고 삭제하고도 남은 pair가 없을 경우에는 짝이 못 이룬 것이므로 
+	//NO를 반환 vector가 비어있으면 pair가 된 거니까 YES로 반환
+	for (int i = 0; i < iCount; ++i)
+	{
+		while (vecPair.at(i).find("()") < 50)
+		{
+			vecPair.at(i).erase(vecPair.at(i).find("()"), 2);
+		}
 
-    return 0;
+		//출력
+		if (vecPair.at(i).empty())
+			cout << "YES" << endl;
+		else 
+			cout << "NO" << endl;
+	}
+
+	return 0;
 }
