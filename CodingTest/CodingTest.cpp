ee5b8e91a5032287faac77a66b483d01df254182
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-28 19:45:59 +0900

    1193번 분수찾기 (설명 추가)

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main()
 {
-    
+    //값 초기화
     int iCount(0);
     int iAdd(0);
     int iIndex(0);
 
     cin >> iCount;
 
+    //iAdd에 값을 더해서 first / second 중에 높은 숫자를 찾음
     for (int i = 1; i <= iCount; ++i)
     {
         iAdd += i;
@@ -22,11 +23,15 @@ int main()
 
     }
 
+    //값 초기화
     int iFirst(0);
     int iSecond(0);
 
+    //이전 최대값의 최대 n번째 값을 찾아서 뺌
     int iPrev = ((iIndex - 1) * (2 + (iIndex - 2))) * 0.5f;
 
+    //iPrev에서 최대값을 뺴고 최소값을 더해서 값을 구함
+    //iIndex가 짝수일 때와 홀수일때 최소,최댓값이 다름
     int iValue = iCount - iPrev - 1;
     if (iIndex % 2 == 0)
     {
@@ -39,6 +44,7 @@ int main()
         iSecond = 1 + iValue;
     }
 
+    //값 출력
     cout << iFirst << "/" << iSecond;
 
 
