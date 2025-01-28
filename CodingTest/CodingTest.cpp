7620da82075a7b6ab74a3eff011a6be2ca619461
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-29 01:35:21 +0900

    1065번 문제 한수 자릿수 하나씩 vector 안에 넣어서 구현 (예제 3번 실패 )

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,73 +1,54 @@
 using namespace std;
 
 #include <iostream>
-#include <stack>
-#include <queue>
-#include <string>
 #include <vector>
 
 int main()
 {
-
-	//값 초기화
-	int iCount(0);
-	cin >> iCount;
-
-	vector<string>vecStr;
-	stack<int> stackInt;
-	queue<int> queueInt;
+	int iNumber(0);
+	int iResult(0);
+	cin >> iNumber;
 
 
-	string strWord;
-	int iNumber(0); 
-
-	//값 대입
-	for (int i = 0; i < iCount; ++i)
+	for (int i = 1; i <= iNumber; ++i)
 	{
-		cin >> strWord;
-		vecStr.push_back(strWord);
+		vector<int> vecInt;
+		int iAdd(0);
+		int iIndex = i;
+		bool bEqual = true;
 
-		if (strWord == "push")
+		while (iIndex != 0)
 		{
-			//queue에 push할 때 들어가는 값을 queue에 저장해서 순차적으로 stack에 저장할 수 있도록 구조
-			cin >> iNumber;
-			queueInt.push(iNumber);
+			vecInt.push_back(iIndex % 10);
+			iIndex /= 10;
 		}
-	}
 
-	//stack기능 구현(라이브러리) / 조건문으로 기능을 나눔
-	for (int i = 0; i < vecStr.size(); ++i)
-	{
-		if (vecStr.at(i) == "top")
+		if (vecInt.size() <= 2)
 		{
-			stackInt.empty() ? cout << -1 << endl : cout << stackInt.top() << endl;
+			iResult++;
+			bEqual = false;
 		}
-		else if (vecStr.at(i) == "size")
+
+
+		for (int i = 0; i < vecInt.size() - 1; ++i)
 		{
-			cout << stackInt.size() << endl;
-		}
-		else if (vecStr.at(i) == "empty")
-		{
-			stackInt.empty() ? cout << 1 << endl : cout << 0 << endl;
-		}
-		else if (vecStr.at(i) == "pop")
-		{
-			if (stackInt.empty())
+			iAdd = vecInt.at(1) - vecInt.at(0);
+
+			if (vecInt.at(i) - vecInt.at(i + 1) != iAdd)
 			{
-				cout << -1 << endl;
-			}
-			else
-			{
-				cout << stackInt.top() << endl;
-				stackInt.pop();
+				bEqual = false;
 			}
 		}
-		else if (vecStr.at(i) == "push")
+
+		if (bEqual)
 		{
-			stackInt.push(queueInt.front());
-			queueInt.pop();
+			iResult++;
 		}
+
 	}
+
+
+	cout << iResult;
 
 	return 0;
 }
