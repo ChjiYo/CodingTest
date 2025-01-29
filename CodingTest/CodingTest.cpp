9b4aa85331f7c08c51fe8132136be57582baf206
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-29 14:11:26 +0900

    1929번 소수 구하기 index로 소수 갯수확인 (시간초과로 인한 실패)

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,40 +1,33 @@
 using namespace std;
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 int main()
 {
 	//값 초기화
-	int iCount(0);
-	int iValue(0);
-	int iResult(0);
-	cin >> iCount;
+	int iMin(0);
+	int iMax(0);
+	int iIndex(0);
 
-	vector<int>vecInt;
+	cin >> iMin >> iMax;
 
-	//값 대입
-	for (int i = 0; i < iCount; ++i)
+	for (int i = iMin; i <= iMax; ++i)
 	{
-		cin >> iValue;
-		vecInt.push_back(iValue);
-	}
-
-	//정렬 알고리즘 (오름차순)
-	sort(vecInt.begin(), vecInt.end());
-
-	//0 , 0 1, 0 1 2 순으로 iResult에 더함
-	for (int i = 0; i < vecInt.size(); ++i)
-	{
-		for (int k = 0; k <= i; ++k)
+		iIndex = 0;
+		for (int k = 1; k <= i; ++k)
 		{
-			iResult += vecInt.at(k);
+			if (i % k == 0)
+			{
+				iIndex++;
+			}
 		}
-	}
 
-	//값 출력
-	cout << iResult;
+		if (iIndex == 2)
+		{
+			cout << i << '\n';
+		}
+
+	}
 
 	return 0;
 }
