4ee6ccde2db9d63a6e409bce855d063fa7e3ec80
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-29 13:29:08 +0900

    11399번 문제 ATM int 변수값에 값을 더해서 해결

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -2,60 +2,38 @@ using namespace std;
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main()
 {
 	//값 초기화
-	int iNumber(0);
+	int iCount(0);
+	int iValue(0);
 	int iResult(0);
-	cin >> iNumber;
+	cin >> iCount;
 
+	vector<int>vecInt;
 
-	for (int i = 1; i <= iNumber; ++i)
+	//값 대입
+	for (int i = 0; i < iCount; ++i)
 	{
-
-		//새 Number가 들어올 때마다 값 초기화 
-		vector<int> vecInt;
-		int iAdd(0);
-		int iIndex = i;
-		bool bEqual = true;
-
-
-		//vector에 자릿수 넣기
-		while (iIndex != 0)
-		{
-			vecInt.push_back(iIndex % 10);
-			iIndex /= 10;
-		}
-
-		// 99이하인 값들은 어차피 다 한수임
-		if (vecInt.size() <= 2)
-		{
-			iResult++;
-			bEqual = false;
-		}
-
-
-		for (int i = 0; i < vecInt.size() - 1; ++i)
-		{
-			iAdd = vecInt.at(0) - vecInt.at(1);
-
-			//하나라도 다르면 false
-			if (vecInt.at(i) - vecInt.at(i + 1) != iAdd)
-			{
-				bEqual = false;
-			}
-		}
-
-		//true일 때만 결과값에 +1;
-		if (bEqual)
-		{
-			iResult++;
-		}
-
+		cin >> iValue;
+		vecInt.push_back(iValue);
 	}
 
-	//출력
+	//정렬 알고리즘 (오름차순)
+	sort(vecInt.begin(), vecInt.end());
+
+	//0 , 0 1, 0 1 2 순으로 iResult에 더함
+	for (int i = 0; i < vecInt.size(); ++i)
+	{
+		for (int k = 0; k <= i; ++k)
+		{
+			iResult += vecInt.at(k);
+		}
+	}
+
+	//값 출력
 	cout << iResult;
 
 	return 0;
