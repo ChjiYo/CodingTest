478af0180bfa7b801f8bdd64067b079e185056b8
JINYOUNG\dkdlt <tick5513@naver.com>
2025-01-30 15:55:33 +0900

    1929번 소수 에라토스테네스의 체 방식으로 해결

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -2,32 +2,29 @@ using namespace std;
 
 #include <iostream>
 
+#define MAX_SIZE 1000001
+
+int arrBool[MAX_SIZE] = {0, 1};
+
 int main()
 {
-	//값 초기화
+	//에라토스테네스의 체 방법
+	//2부터 시작해서 해당 수를 제외한 해당 수의 배수들을 제거한다.
+
+	//제곱 수를 다 1로 바꿈 
+
+
 	int iMin(0);
 	int iMax(0);
-	int iIndex(0);
-
 	cin >> iMin >> iMax;
 
+	for(int i = 2; i <= iMax; i++)
+		for (int k = 2; i * k <= iMax; k++)
+			arrBool[i * k] = 1;
+
+	//0인 애들 출력
 	for (int i = iMin; i <= iMax; ++i)
-	{
-		iIndex = 0;
-		for (int k = 1; k <= i; ++k)
-		{
-			if (i % k == 0)
-			{
-				iIndex++;
-			}
-		}
-
-		if (iIndex == 2)
-		{
-			cout << i << '\n';
-		}
-
-	}
+		if (!arrBool[i]) cout << i << '\n';
 
 	return 0;
 }
