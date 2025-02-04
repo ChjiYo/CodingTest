86ea7709988448dcc35e24bbeabf92519ac0d7d9
JinYoung <tick5513@naver.com>
2025-02-04 16:55:47 +0900

    1003번 파보나치 함수 문제 다이나믹 프로그래밍으로 다운 업 방식으로 구현

--- CodingTest/Project1/CodingTest.cpp
+++ CodingTest/Project1/CodingTest.cpp
@@ -1,40 +1,40 @@
 using namespace std;
 
 #include <iostream>
+#include <vector>
 
-#define MAX_SIZE 12
+#define MAX_SIZE 41
 
 int main()
 {
 	//값 초기화
-	int iArrAdd[MAX_SIZE];
-	iArrAdd[0] = 1;
-	iArrAdd[1] = 1;
-	iArrAdd[2] = 2;
+	vector<pair<int, int>> vPair;
+	vPair.resize(MAX_SIZE);
+	vPair.at(0).first = 1;
+	vPair.at(1).second = 1;
 
-	//다이나믹 프로그래밍으로 iArrAdd[] 배열 안에 값 대입
-	for (int i = 3; i < 12; ++i)
+	//다이나믹 프로그래밍을 통한 다운업 방식의 값 대입 
+	for (int i = 2; i < MAX_SIZE; ++i)
 	{
-		iArrAdd[i] = iArrAdd[i - 1] + iArrAdd[i - 2] + iArrAdd[i - 3];
+		vPair.at(i).first += vPair.at(i - 1).first + vPair.at(i - 2).first;
+		vPair.at(i).second += vPair.at(i - 1).second + vPair.at(i - 2).second;
 	}
 
-	//값 초기화
 	int iCount(0);
 	cin >> iCount;
 
 	int iNumber(0);
 
-	//값 출력
+	//값 출력 
 	for (int i = 0; i < iCount; ++i)
 	{
 		cin >> iNumber;
-		cout << iArrAdd[iNumber] << '\n';
+		cout << vPair.at(iNumber).first << " " << vPair.at(iNumber).second << '\n';
 	}
 
-	//1,2,3으로 더해지는 숫자를 찾는 것이기 떄문에
-	//iArrAdd[i - 3] -> 각 경우의 수에 3을 더하면 iArrAdd[i]의 갯수
-	//iArrAdd[i - 2] -> 각 경우의 수에 2을 더하면 iArrAdd[i]의 갯수
-	//iArrAdd[i - 1] -> 각 경우의 수에 1을 더하면 iArrAdd[i]의 갯수
+	//파보나치라 n번째 0의 개수와 1의 개수를 세는 방법은
+	// Func(n) = Func(n - 1) + Func(n - 2)이기 때문에 
+	// Func(n - 1) + Func(n - 2)의 0과 1의 개수를 더하면 결과값이 나옴
 
 	return 0;
 }
