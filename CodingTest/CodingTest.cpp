ddb4a8dc7bded790051cf66a36ce7a181e35ab9d
JinYoung <tick5513@naver.com>
2025-02-04 10:53:00 +0900

    커밋

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,44 +1,20 @@
 using namespace std;
 
 #include <iostream>
-#include <algorithm>
 
-#define MAX_SIZE 1000000
-int arr[MAX_SIZE];
+const int MAX = 100;
+long long dp[MAX];
+
+long long fibonacci(int n)
+{
+	if (n <= 1) return n;
+	if (dp[n] != 0) return dp[n];
+	return dp[n] = fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 
 int main()
 {
-	int iNumber(0);
-	cin >> iNumber;
-
-	// 2 3 4 5 6 7 8 9 10
-	for (int i = 2; i <= iNumber; ++i)
-	{
-		//1. 1�� ����
-		arr[i] = arr[i - 1] + 1;
-
-		//2. 2�� ����������
-		if (i % 2 == 0)
-		{
-			//2 4 6 8 10
-			arr[i] = min(arr[i] , arr[i / 2] + 1);
-			int b = 10;
-		}
-
-		//3. 3���� ����������
-		if (i % 3 == 0)
-		{
-			//3 5 7 9
-			arr[i] = min(arr[i], arr[i / 3] + 1);
-			int b = 10;
-		}
-		int a = 10;
-	}
-	
-	
-
-
-	cout << arr[iNumber];
+	cout << fibonacci(50) << endl;
 	return 0;
-
 }
